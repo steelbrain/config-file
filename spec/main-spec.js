6b869a365d45f834b4c41f3936b423b52f1152cd
Steel Brain <de.steelbrain@gmail.com>
2018-01-10 00:54:06 +0500

    :fire: Remove append tests

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -6,7 +6,7 @@ import copy from 'sb-copy'
 import promisify from 'sb-promisify'
 import { it, beforeEach, afterEach } from 'jasmine-fix'
 
-import ConfigFile from '../src'
+import getConfigFileReal from '../src'
 
 const rimraf = promisify(require('rimraf'))
 const mkdirp = promisify(require('mkdirp'))
@@ -14,9 +14,7 @@ const mkdirp = promisify(require('mkdirp'))
 describe('ConfigFile', function() {
   const fixtures = Path.join(__dirname, 'fixtures')
   const directory = Path.join(fixtures, 'ignored')
-  const nonExistent = Path.join(directory, 'file-1')
   const tempPath1 = Path.join(directory, 'temp-1')
-  const tempPath2 = Path.join(directory, 'temp-2')
   const configPath = Path.join(directory, 'config.json')
 
   beforeEach(async function() {
@@ -28,15 +26,23 @@ describe('ConfigFile', function() {
   })
 
   function getConfigFile(...args: Array<any>) {
-    return ConfigFile.get(...args)
+    return getConfigFileReal(...args)
   }
-  //
-  it('creates empty if file is non existent', async function() {
-    expect(await FS.exists(nonExistent)).toBe(false)
-    await getConfigFile(nonExistent, null, { createIfNonExistent: true })
-    expect(await FS.readFile(nonExistent, 'utf8')).toBe('{}\n')
-  })
 
+  describe('pretty printing', function() {
+    it('pretty prints by default', async function() {
+      const configFile = await getConfigFile(tempPath1, null)
+      await configFile.set('some', 'thing')
+      expect(await FS.readFile(tempPath1, 'utf8')).toBe('{\n  "some": "thing"\n}\n')
+    })
+    it('can disable pretty printing if we tell it to', async function() {
+      const configFile = await getConfigFile(tempPath1, null, {
+        prettyPrint: false,
+      })
+      await configFile.set('some', 'thing')
+      expect(await FS.readFile(tempPath1, 'utf8')).toBe('{"some":"thing"}\n')
+    })
+  })
   describe('Sync APIs', function() {
     it('returns full object if get is empty', async function() {
       const configFile = await getConfigFile(configPath)
@@ -62,7 +68,7 @@ describe('ConfigFile', function() {
       const configFile = await getConfigFile(configPath)
 
       // write a new object
-      expect(configFile.getSync('new.deep.prop')).toEqual(null)
+      expect(configFile.getSync('new.deep.prop')).toEqual(undefined)
       configFile.setSync('new.deep.prop', 50)
       expect(configFile.getSync('new')).toEqual({
         deep: {
@@ -70,7 +76,7 @@ describe('ConfigFile', function() {
         },
       })
       // add new prop to object
-      expect(configFile.getSync('new.deep.newProp')).toEqual(null)
+      expect(configFile.getSync('new.deep.newProp')).toEqual(undefined)
       configFile.setSync('new.deep.newProp', true)
       expect(configFile.getSync('new')).toEqual({
         deep: {
@@ -80,52 +86,25 @@ describe('ConfigFile', function() {
       })
 
       // write a new array
-      expect(configFile.getSync('new.deep.array')).toEqual(null)
+      expect(configFile.getSync('new.deep.array')).toEqual(undefined)
       configFile.setSync('new.deep.array', [1, 3, 5])
       expect(configFile.getSync('new.deep.array')).toEqual([1, 3, 5])
 
       // add item at specific index in array
-      expect(configFile.getSync('new.deep.array.10')).toEqual(null)
+      expect(configFile.getSync('new.deep.array.10')).toEqual(undefined)
       configFile.setSync('new.deep.array.10', 50)
-      expect(configFile.getSync('new.deep.array')).toEqual([1, 3, 5, null, null, null, null, null, null, null, 50])
-    })
-
-    it('appens arrays', async function() {
-      const configFile = await getConfigFile(configPath)
-      configFile.appendSync('array', true)
-      expect(configFile.getSync('array')).toEqual(['1', 2, '5', true])
+      expect(configFile.getSync('new.deep.array')).toEqual([1, 3, 5, undefined, undefined, undefined, undefined, undefined, undefined, undefined, 50])
     })
 
     it('deletes stuff', async function() {
       const configFile = await getConfigFile(configPath)
       configFile.deleteSync('array')
-      expect(configFile.getSync('array')).toEqual(null)
+      expect(configFile.getSync('array')).toEqual(undefined)
       expect(configFile.getSync('object.deep.ha')).toEqual(1)
       configFile.deleteSync('object.deep.ha')
       expect(configFile.getSync('object.deep')).toEqual({
         prop: 'yes',
       })
-    })
-
-    it('pretty prints by default', async function() {
-      await getConfigFile(tempPath1, { some: 'thing' }, { createIfNonExistent: true })
-      expect(await FS.readFile(tempPath1, 'utf8')).toBe('{\n  "some": "thing"\n}\n')
-    })
-    it('can disable pretty printing if we tell it to', async function() {
-      await getConfigFile(tempPath1, { some: 'thing' }, {
-        prettyPrint: false,
-        createIfNonExistent: true,
-      })
-      expect(await FS.readFile(tempPath1, 'utf8')).toBe('{"some":"thing"}\n')
-    })
-    it('does not throw if file does not exist and createIfNonExistent is false', async function() {
-      const configFile = await getConfigFile(tempPath2, {
-        some: 'wow',
-      }, {
-        createIfNonExistent: false,
-      })
-      expect(await configFile.get()).toEqual({ some: 'wow' })
-      expect(await FS.exists(tempPath2)).toBe(false)
     })
   })
   describe('Async APIs', function() {
@@ -153,7 +132,7 @@ describe('ConfigFile', function() {
       const configFile = await getConfigFile(configPath)
 
       // write a new object
-      expect(await configFile.get('new.deep.prop')).toEqual(null)
+      expect(await configFile.get('new.deep.prop')).toEqual(undefined)
       await configFile.set('new.deep.prop', 50)
       expect(await configFile.get('new')).toEqual({
         deep: {
@@ -161,7 +140,7 @@ describe('ConfigFile', function() {
         },
       })
       // add new prop to object
-      expect(await configFile.get('new.deep.newProp')).toEqual(null)
+      expect(await configFile.get('new.deep.newProp')).toEqual(undefined)
       await configFile.set('new.deep.newProp', true)
       expect(await configFile.get('new')).toEqual({
         deep: {
@@ -171,52 +150,25 @@ describe('ConfigFile', function() {
       })
 
       // write a new array
-      expect(await configFile.get('new.deep.array')).toEqual(null)
+      expect(await configFile.get('new.deep.array')).toEqual(undefined)
       await configFile.set('new.deep.array', [1, 3, 5])
       expect(await configFile.get('new.deep.array')).toEqual([1, 3, 5])
 
       // add item at specific index in array
-      expect(await configFile.get('new.deep.array.10')).toEqual(null)
+      expect(await configFile.get('new.deep.array.10')).toEqual(undefined)
       await configFile.set('new.deep.array.10', 50)
-      expect(await configFile.get('new.deep.array')).toEqual([1, 3, 5, null, null, null, null, null, null, null, 50])
-    })
-
-    it('appens arrays', async function() {
-      const configFile = await getConfigFile(configPath)
-      await configFile.append('array', true)
-      expect(await configFile.get('array')).toEqual(['1', 2, '5', true])
+      expect(await configFile.get('new.deep.array')).toEqual([1, 3, 5, undefined, undefined, undefined, undefined, undefined, undefined, undefined, 50])
     })
 
     it('deletes stuff', async function() {
       const configFile = await getConfigFile(configPath)
       await configFile.delete('array')
-      expect(await configFile.get('array')).toEqual(null)
+      expect(await configFile.get('array')).toEqual(undefined)
       expect(await configFile.get('object.deep.ha')).toEqual(1)
       await configFile.delete('object.deep.ha')
       expect(await configFile.get('object.deep')).toEqual({
         prop: 'yes',
       })
-    })
-
-    it('pretty prints by default', async function() {
-      await getConfigFile(tempPath1, { some: 'thing' }, { createIfNonExistent: true })
-      expect(await FS.readFile(tempPath1, 'utf8')).toBe('{\n  "some": "thing"\n}\n')
-    })
-    it('can disable pretty printing if we tell it to', async function() {
-      await getConfigFile(tempPath1, { some: 'thing' }, {
-        prettyPrint: false,
-        createIfNonExistent: true,
-      })
-      expect(await FS.readFile(tempPath1, 'utf8')).toBe('{"some":"thing"}\n')
-    })
-    it('does not throw if file does not exist and createIfNonExistent is false', async function() {
-      const configFile = await getConfigFile(tempPath2, {
-        some: 'wow',
-      }, {
-        createIfNonExistent: false,
-      })
-      expect(configFile.getSync()).toEqual({ some: 'wow' })
-      expect(await FS.exists(tempPath2)).toBe(false)
     })
   })
 })
